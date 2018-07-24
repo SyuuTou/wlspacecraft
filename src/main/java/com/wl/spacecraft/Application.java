7ae24f16b0db002503ce9841bf7b01fae628d89a
syuutousan <1152750238@qq.com>
2018-07-24 14:12:31 +0800

    配置文件修改

--- src/main/java/com/wl/spacecraft/Application.java
+++ src/main/java/com/wl/spacecraft/Application.java
@@ -7,6 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 
 /**
@@ -17,7 +22,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 
 @MapperScan(basePackages = "com.wl.spacecraft.mapper")
-public class Application {
+public class Application extends WebMvcConfigurerAdapter {
   
     // 启动的时候要注意，由于我们在controller中注入了RestTemplate，所以启动的时候需要实例化该类的一个实例
     @Autowired
@@ -26,7 +31,35 @@ public class Application {
     // 使用RestTemplateBuilder来实例化RstTemplate对象，spring默认已经注入了RestTemplateBuilder实例
     @Bean
     public RestTemplate restTemplate() {
+
         return builder.build();
+    }
+
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        return corsConfiguration;
+    }
+    /**
+     * 跨域过滤器
+     * @return
+     */
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildConfig()); // 4
+        return new CorsFilter(source);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowCredentials(true)
+                .allowedHeaders("*")
+                .allowedOrigins("*")
+                .allowedMethods("*");
     }
 
     public static void main(String[] args) {
